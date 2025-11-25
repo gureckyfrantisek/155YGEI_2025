0f1692004579bd34622f955f5688907582be831d
František Gurecký <gurecfra@cvut.cz>
2025-11-25 10:44:49 +0100

    feat: Add Dijkstra
    Dijsktra iterates through different cost graphs, but the data is incorrect, many nodes are connected only to their one of the neighbours

--- Uloha3/main.py
+++ Uloha3/main.py
@@ -1,7 +1,49 @@
 from lines_to_graph2 import *
 from pathlib import Path
 
-# Implement Dijskra's algo here
+def dijkstra(G, start):
+    '''
+        Dijkstra's algorithm
+        G - graph on which we are operating
+        start - the starting node
+        
+        returns a list of predecessors
+    '''
+    
+    # Init variables
+    n = len(G)
+    pred = [None] * (n + 1)
+    dist = [inf] * (n + 1)
+    prioQ = PriorityQueue()
+    
+    # Put the start into queue and update the distance
+    prioQ.put((0, start))
+    dist[start] = 0
+    
+    # While we have nodes
+    while not prioQ.empty():
+        # Get the top prio node
+        du, u = prioQ.get()
+        
+        # For all it's neighbours
+        for v, wuv in G[u].items():
+            # Relax the node if there's a shorter path
+            if dist[v] > dist[u] + wuv:
+                dist[v] = dist[u] + wuv
+                pred[v] = u
+                prioQ.put((dist[v], v))
+                
+    return pred
+
+def rectPath(pred, u, v):
+    path = []       # Empty path
+    
+    while u != v and v != None:
+        path.append(v)
+        v = pred[v]
+        
+    path.append(v)
+    return path
 
 def main():
     # First translate all line data to graphs
@@ -10,6 +52,19 @@ def main():
 
     for file in data_dir.iterdir():
         graphs.append(convertToGraph(file))
+    
+    # Run Dijsktra on all costs
+    predecessors = [[]] * len(graphs)
+    paths = [[]] * len(graphs)
+    
+    for i in range(len(graphs)):
+        predecessors[i] = dijkstra(graphs[i], 1)
+        
+        paths[i] = rectPath(predecessors[i], 1, 2)
+    
+        print(paths[i])
+    
+    print(paths)
 
 if __name__ == "__main__":
     main()
